a62e209be49fb66b1401005f3822d484efc23d55
VladyslavPrytula <vladyslav.prytula@gmail.com>
2014-03-20 20:23:18 +0200

    Initial commit

---
+++ src/main/java/com/google/android/gcm/demo/entity/Invoice.java
@@ -0,0 +1,97 @@
+package com.google.android.gcm.demo.entity;
+
+import org.eclipse.persistence.nosql.annotations.DataFormatType;
+import org.eclipse.persistence.nosql.annotations.Field;
+import org.eclipse.persistence.nosql.annotations.NoSql;
+
+
+import javax.persistence.*;
+import java.sql.Date;
+
+/**
+ * Created by arkadii.tetelman on 3/13/14.
+ */
+
+@Entity
+@NoSql(dataFormat= DataFormatType.MAPPED)
+public class Invoice {
+
+    @Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Field(name = "invoiceId")
+	long invoiceId;
+
+	@Basic
+	private String invoiceName;
+	@Basic
+    private Double invoiceAmount;
+	@Basic
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date invoiceSubmitedTS;
+	@Basic
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date invoiceCompletedTS;
+	@Column
+    private  User user;
+
+	public Invoice() {
+	}
+
+	public Invoice(long invoiceId, String invoiceName, Double invoiceAmount,
+			Date invoiceSubmitedTS, Date invoiceCompletedTS, User user) {
+		this.invoiceId = invoiceId;
+		this.invoiceName = invoiceName;
+		this.invoiceAmount = invoiceAmount;
+		this.invoiceSubmitedTS = invoiceSubmitedTS;
+		this.invoiceCompletedTS = invoiceCompletedTS;
+		this.user = user;
+	}
+
+	public long getInvoiceId() {
+		return invoiceId;
+	}
+
+	public void setInvoiceId(long invoiceId) {
+		this.invoiceId = invoiceId;
+	}
+
+	public String getInvoiceName() {
+		return invoiceName;
+	}
+
+	public void setInvoiceName(String invoiceName) {
+		this.invoiceName = invoiceName;
+	}
+
+	public Double getInvoiceAmount() {
+		return invoiceAmount;
+	}
+
+	public void setInvoiceAmount(Double invoiceAmount) {
+		this.invoiceAmount = invoiceAmount;
+	}
+
+	public Date getInvoiceSubmitedTS() {
+		return invoiceSubmitedTS;
+	}
+
+	public void setInvoiceSubmitedTS(Date invoiceSubmitedTS) {
+		this.invoiceSubmitedTS = invoiceSubmitedTS;
+	}
+
+	public Date getInvoiceCompletedTS() {
+		return invoiceCompletedTS;
+	}
+
+	public void setInvoiceCompletedTS(Date invoiceCompletedTS) {
+		this.invoiceCompletedTS = invoiceCompletedTS;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+}
---
+++ src/main/java/com/google/android/gcm/demo/entity/User.java
@@ -0,0 +1,69 @@
+package com.google.android.gcm.demo.entity;
+
+import org.eclipse.persistence.nosql.annotations.DataFormatType;
+import org.eclipse.persistence.nosql.annotations.Field;
+import org.eclipse.persistence.nosql.annotations.NoSql;
+
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+/** * Created by arkadii.tetelman on 3/19/14. */
+@Entity
+@NoSql(dataFormat= DataFormatType.MAPPED)
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Field(name = "userId")
+	private long userId;
+	@Basic
+	private String userEmail;
+	@Basic
+	private String userPassword;
+	@Basic
+    @NotNull
+	private String googleAPIKey;
+
+	public User(long userId, String userEmail, String userPassword,
+			String googleAPIKey) {
+		this.userId = userId;
+		this.userEmail = userEmail;
+		this.userPassword = userPassword;
+		this.googleAPIKey = googleAPIKey;
+	}
+
+	public User() {
+	}
+
+	public long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(long userId) {
+		this.userId = userId;
+	}
+
+	public String getUserEmail() {
+		return userEmail;
+	}
+
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+
+	public String getUserPassword() {
+		return userPassword;
+	}
+
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+
+	public String getGoogleAPIKey() {
+		return googleAPIKey;
+	}
+
+	public void setGoogleAPIKey(String googleAPIKey) {
+		this.googleAPIKey = googleAPIKey;
+	}
+}
